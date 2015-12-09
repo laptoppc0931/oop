69e7988ca6c6f56985a7d98084c39ed1352afe37
laptoppc0931 <laptoppc0931@gmail.com>
2015-12-09 15:44:24 +0100

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package listen;
 
 /**
@@ -14,5 +19,57 @@ public class BinaryTree {
     public BinaryTree(BinaryTreeNode root) {
         this.root = root;
     }
-// ...
+
+    public void printPreorder() {
+        // start with root
+        printPreorder(root);
+    }
+
+    private void printPreorder(BinaryTreeNode tn) {
+// base case: empty subtree
+        if (tn == null) {
+            return;
+        }
+// visit current node
+        System.out.print(tn.getEntry() + " ");
+// visit left child
+        printPreorder(tn.getLeftChild());
+// visit right child
+        printPreorder(tn.getRightChild());
+    }
+
+    public void printInorder() {
+        // start with root
+        printInorder(root);
+    }
+
+    private void printInorder(BinaryTreeNode tn) {
+        if (tn == null) {
+            return;
+        }
+        // visit left child
+        printInorder(tn.getLeftChild());
+        // visit current node
+        System.out.print(tn.getEntry() + " ");
+        // visit right child
+        printInorder(tn.getRightChild());
+
+    }
+
+    public void printPostorder() {
+        // start with root
+        printInorder(root);
+    }
+
+    private void printPostorder(BinaryTreeNode tn) {
+        if (tn == null) {
+            return;
+        }
+        // visit left child
+        printPostorder(tn.getLeftChild());
+        // visit right child
+        printPostorder(tn.getRightChild());
+        // visit current node
+        System.out.print(tn.getEntry() + " ");
+    }
 }
