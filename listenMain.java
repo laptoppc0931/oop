fc09ca74e426a13560ea2f8c471027be47ac719b
laptoppc0931 <laptoppc0931@gmail.com>
2015-12-09 15:15:49 +0100

    Update and rename listen.java to listenMain.java

--- listenMain.java
+++ listenMain.java
@@ -9,7 +9,7 @@ package listen;
  *
  * @author v990461
  */
-public class listen {
+public class listenMain {
 
     /**
      * @param args the command line arguments
@@ -30,12 +30,11 @@ public class listen {
 //        liste.printList();
 //        System.out.println(" ");
 //        liste.printReverseList();
-        
+
 //        LinkedList liste = new LinkedList();
 //        for (int i = 1; i <= 8; i++) {
 //            liste.add(i);
 //        }
-        
         //liste.remove(5);
         //liste.printList();
 //        boolean contains = liste.contains(5);
@@ -49,22 +48,29 @@ public class listen {
 //        }
 //        int size = liste.size();
 //        System.out.println(size);
-        
-        SortedList sort = new SortedList();
-        sort.add(2);
-        sort.add(8);
-        sort.add(3);
-        
-        //sort.printList();
-        System.out.println(" ");
-        
-        sort.add(5);
-        //sort.printList();
-        
-        boolean enth = sort.contains(8);
-        System.out.println(enth);
-        
-        
+//        SortedList sort = new SortedList();
+//        sort.add(2);
+//        sort.add(8);
+//        sort.add(3);
+//        
+//        //sort.printList();
+//        System.out.println(" ");
+//        
+//        sort.add(5);
+//        //sort.printList();
+//        
+//        boolean enth = sort.contains(8);
+//        System.out.println(enth);
+        BinaryTreeNode acht = new BinaryTreeNode(8);
+        BinaryTreeNode vier = new BinaryTreeNode(4);
+        BinaryTreeNode fuenf = new BinaryTreeNode(5);
+        BinaryTreeNode neun = new BinaryTreeNode(9);
+        BinaryTreeNode sechs = new BinaryTreeNode(6, null, fuenf);
+        BinaryTreeNode eins = new BinaryTreeNode(1, vier, acht);
+        BinaryTreeNode zwei = new BinaryTreeNode(2, neun, sechs);
+        BinaryTreeNode sieben = new BinaryTreeNode(7, zwei, eins);
+        BinaryTree t = new BinaryTree(sieben);
+
     }
-    
+
 }
