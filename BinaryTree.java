e08260a79649d255aa02e8a40f22cb1db6e4e0ee
laptoppc0931 <laptoppc0931@gmail.com>
2015-12-09 15:01:03 +0100

    Create BinaryTree.java

---
+++ BinaryTree.java
@@ -0,0 +1,18 @@
+package listen;
+
+/**
+ *
+ * @author v990461
+ */
+public class BinaryTree {
+
+    private BinaryTreeNode root;
+
+    public BinaryTree() {
+    }
+
+    public BinaryTree(BinaryTreeNode root) {
+        this.root = root;
+    }
+// ...
+}
