fde14567f01e44b8644ebdb6d70d81be033ad9bc
laptoppc0931 <laptoppc0931@gmail.com>
2015-12-09 10:48:39 +0100

    Create DoublyLinkedListNode.java

---
+++ DoublyLinkedListNode.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listen;
+
+/**
+ *
+ * @author v990461
+ */
+public class DoublyLinkedListNode {
+
+    private String entry;
+    private DoublyLinkedListNode next;
+    private DoublyLinkedListNode prev;
+
+    
+} // EOF
